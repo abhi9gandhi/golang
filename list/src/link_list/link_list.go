1c73877ad913845d14f805c635e8593a23ecc942
Abhinav Gandhi <abhi9.gandhi@gmail.com>
2017-08-21 13:27:37 -0700

    Link list add and delete for circular and doubly

--- list/src/link_list/link_list.go
+++ list/src/link_list/link_list.go
@@ -2,6 +2,12 @@ package link_list
 
 import "fmt"
 
+const (
+	SINGLY = 1
+	DOUBLY = 2
+	DOUBLY_CIRCULAR = 3
+)
+
 type node struct {
 	data int
 	next *node
@@ -10,7 +16,16 @@ type node struct {
 
 var head *node = nil
 
-func Add_list(data int) {
+
+type add_list func (data int)
+type delete_list func (data int)
+type print_list func ()
+
+var Add_callbacks = map[int] add_list {1: add_singly_list, 2: add_doubly_list, 3: add_doubly_circular_list }
+var Delete_callbacks = map[int] delete_list {1: delete_singly_list, 2: delete_doubly_list, 3: delete_doubly_circular_list }
+var Print_callbacks = map[int] print_list {1: print_singly_list, 2: print_doubly_list, 3: print_doubly_circular_list }
+
+func add_singly_list(data int) {
 	var iter *node = head
 	
 	new_node := new(node)
@@ -28,7 +43,50 @@ func Add_list(data int) {
    return 		
 }
 
-func Delete_list(data int) {
+func add_doubly_list(data int) {
+	var iter *node = head
+	
+	new_node := new(node)
+	new_node.data = data
+	new_node.next = nil
+	new_node.prev = nil
+		
+	if head == nil {
+		head = new_node
+	} else {	
+		for iter.next != nil {
+			iter = iter.next
+		}
+		iter.next = new_node
+		new_node.prev = iter
+	}
+   return 		
+}
+
+func add_doubly_circular_list(data int) {
+	var iter *node = head
+	
+	new_node := new(node)
+	new_node.data = data
+		
+	if head == nil {
+		head = new_node
+		new_node.next = head
+		new_node.prev = head
+	} else {	
+		for iter.next != head {
+			iter = iter.next
+		}
+		
+		new_node.next = iter.next
+		iter.next.prev = new_node
+		iter.next = new_node
+		new_node.prev = iter
+	}
+   return 		
+}
+
+func delete_singly_list(data int) {
 	var iter *node = head
 	var is_found bool = false
 	
@@ -58,7 +116,102 @@ func Delete_list(data int) {
 	return
 }
 
-func Print_list() {
+func delete_doubly_list(data int) {
+	var iter *node = head
+	var is_found bool = false
+	
+	if head == nil {
+		fmt.Printf("List is empty")
+		return
+	}
+	
+	if head.data == data {
+		if head.next != nil {
+			head.next.prev = nil
+		}
+		head = head.next
+		return
+	}
+	
+	iter = iter.next
+	
+	for iter != nil {
+		if iter.data == data {
+			is_found = true
+			if iter.next != nil {
+				iter.next.prev = iter.prev
+			} 
+			
+			iter.prev.next = iter.next
+			break
+		}
+	
+		iter = iter.next
+	}
+	
+	if is_found == false {
+		fmt.Printf("Could not find element %d in existing list", data)
+	}
+	
+	return
+}
+
+func delete_doubly_circular_list(data int) {
+	var iter *node = head
+	var is_found bool = false
+	
+	if head == nil {
+		fmt.Printf("List is empty")
+		return
+	}
+	
+	if head.data == data {
+		if head.next == head {
+			head = nil
+			return
+		}
+		head.prev.next = head.next
+		head.next.prev = head.prev
+		head = head.next
+		return
+	}
+	
+	iter = head.next
+	
+	for iter != head {
+		if iter.data == data {
+			is_found = true
+			
+			iter.next.prev = iter.prev
+			iter.prev.next = iter.next
+			break
+		}
+		iter = iter.next
+	}
+	
+	if is_found == false {
+		fmt.Printf("Could not find element %d in existing list", data)
+	}
+	
+	return
+}
+
+func print_singly_list() {
+	var iter *node = head
+	
+	fmt.Printf("\nList data: \n")
+	if head == nil {
+		return
+	} 
+	
+	for iter != nil {
+		fmt.Printf(" %d ", iter.data)
+		iter = iter.next
+	}
+	return
+}
+
+func print_doubly_list() {
 	var iter *node = head
 	
 	fmt.Printf("\nList data: \n")
@@ -73,3 +226,20 @@ func Print_list() {
 	return
 } 
 
+func print_doubly_circular_list() {
+	var iter *node = head
+	
+	fmt.Printf("\nList data: \n")
+	if head == nil {
+		return
+	} 
+	
+	for iter.next != head {
+		fmt.Printf(" %d ", iter.data)
+		iter = iter.next
+	}
+	
+	fmt.Printf(" %d ", iter.data)
+	
+	return
+}
--- list/src/main/main.go
+++ list/src/main/main.go
@@ -1,27 +1,45 @@
 package main
+
 import "fmt"
 import "link_list"
+import "os"
 
 func main() {
 	var option, data int = 0, 0
+	var is_exit bool = false
+	var list_type int = link_list.SINGLY
 	for {
-		fmt.Println("\nEnter the option: 1- Add 2- Delete 3-Print\n")
-		fmt.Scanf("%d", &option)
-		
-		switch option {
+			fmt.Println("\nEnter the List type: 1- Singly 2- Doubly 3- Doubly Circular 4- Exit\n")
+			fmt.Scanf("%d", &list_type)
+		if list_type == 4 {
+			os.Exit(0)
+		}
+		is_exit = false
+		for {
+			fmt.Println("\nEnter the option: 1- Add 2- Delete 3-Print 4- Exit\n")
+			fmt.Scanf("%d", &option)
+
+			switch option {
 			case 1:
 				fmt.Printf("Enter the data to add in list: ")
 				fmt.Scanf("%d", &data)
-				link_list.Add_list(data)
-			break
+				link_list.Add_callbacks[list_type](data)
+				break
 			case 2:
 				fmt.Printf("Enter the data to be delete from list: ")
 				fmt.Scanf("%d", &data)
-				link_list.Delete_list(data)
-			break
+				link_list.Delete_callbacks[list_type](data)
+				break
 			case 3:
-				link_list.Print_list()
-			break
-		}		
-	}	
+				link_list.Print_callbacks[list_type]()
+				break
+			case 4:
+				is_exit = true
+				break
+			}
+			if is_exit == true {
+				break;
+			}
+		}
+	}
 }
